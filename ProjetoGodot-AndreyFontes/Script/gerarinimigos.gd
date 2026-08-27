09354f51abc9561b08a6980485514dc5b25cb933
unknown <theefontess@gmail.com>
2026-08-27 08:54:43 -0300

    Implementação novos inimigos/assets

--- ProjetoGodot-AndreyFontes/Script/gerarinimigos.gd
+++ ProjetoGodot-AndreyFontes/Script/gerarinimigos.gd
@@ -1,16 +1,16 @@
 extends Node3D
 
 
-@onready var coin = preload("res://Prefab/coin.tscn")
+@onready var caranguejo = preload("res://Prefab/caranguejo.tscn")
 
-var tamanho_mapa = Vector2(150.0, 150.0)
+var tamanho_mapa = Vector2(90.0, 90.0)
+
 @onready var rng := RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void: # Simular o Start
-	add_to_group("coin")
 	for i in range(10): # Cria 10 objetos
-		var instancia = coin.instantiate() # Cria o Objeto
+		var instancia = caranguejo.instantiate() # Cria o Objeto
 		
 		var x = rng.randf_range(-tamanho_mapa.x, tamanho_mapa.x)
 		var z = rng.randf_range(-tamanho_mapa.y, tamanho_mapa.y)
